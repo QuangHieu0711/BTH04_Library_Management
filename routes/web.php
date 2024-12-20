786382469ef21eb4d9721cee059908550b1e3fae
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2024-12-20 17:10:31 +0700

    Create_View_Home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReaderController;
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get("/", [ReaderController::class,"home"])->name('home');
+    Route::prefix('readers')->group(function () {
+        Route::get('/', [ReaderController::class, 'index'])->name('reader.index');
+        Route::get('/create', [ReaderController::class, 'create'])->name('reader.create');
+        Route::post('/store', [ReaderController::class, 'store'])->name('reader.store');
+        Route::get('/{name}', [ReaderController::class, 'show'])->name('reader.show');
+        Route::get('/{id}/edit', [ReaderController::class, 'edit'])->name('reader.edit');
+        Route::put('/{id}', [ReaderController::class, 'update'])->name('reader.update');
 });
