fac5f20183d8b3a6fc0f7c496ba0291c2700f7a7
ThuyLienLuong <luonglien332@gmail.com>
2024-12-22 01:33:51 +0700

    update history_borrow

--- resources/views/borrows/history.blade.php
+++ resources/views/borrows/history.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('title', 'Lịch sử mượn trả sách')
@@ -6,25 +5,38 @@
 @section('content')
 <div class="container-xl">
     <h1>Lịch sử mượn trả sách của {{ $reader->name }}</h1>
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Tên sách</th>
-                <th>Ngày mượn</th>
-                <th>Ngày trả</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($borrows as $borrow)
-            <tr>
-                <td>{{ $borrow->id }}</td>
-                <td>{{ $borrow->book->name }}</td>
-                <td>{{ $borrow->borrow_date }}</td>
-                <td>{{ $borrow->return_date }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tên sách</th>
+                    <th>Ngày mượn</th>
+                    <th>Ngày trả</th>
+                    <th>Trạng thái</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($borrows as $borrow)
+                <tr>
+                    <td>{{ $borrow->id }}</td>
+                    <td>{{ $borrow->book->name }}</td>
+                    <td>{{ $borrow->borrow_date }}</td>
+                    <td>{{ $borrow->return_date }}</td>
+                    <td>
+                        @if ($borrow->status == 0)
+                        <span class="btn btn-borrowing">Đang mượn</span>
+                        @else
+                        <span class="btn btn-returned"><i class="bi bi-check-circle"></i> Đã trả</span>
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end mt-3">
+        <a href="{{ route('borrow.index') }}" class="btn btn-primary">Quay lại</a>
+    </div>
 </div>
 @endsection
