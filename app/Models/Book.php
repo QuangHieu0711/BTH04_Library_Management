efc955c651cd8004d3a4f255ef14b86cdfa60517
ThuyLienLuong <luonglien332@gmail.com>
2024-12-21 14:42:17 +0700

    update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Database\Eloquent\Relations\HasMany;;
 
 class Book extends Model
 {
@@ -17,5 +17,9 @@ class Book extends Model
         'price',
         'quantity',
     ];
+    public function borrows(): HasMany
+    {
+        return $this->hasMany(Borrow::class);
+    }
     
 }
